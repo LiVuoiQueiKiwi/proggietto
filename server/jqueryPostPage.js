b0a93a5e09718f280c0927dd959a069abff97d3a
Saimonz <grillini.simo@gmail.com>
2020-01-09 18:37:04 +0100

    Test aggiornato con anche richiesta GET.

--- server/jqueryPostPage.js
+++ server/jqueryPostPage.js
@@ -5,10 +5,11 @@ module.exports =
         <script src = 'https://code.jquery.com/jquery-3.4.1.min.js'></script>
     </head>
     <body>
-        <button id = 'test-btn'>Send POST</button>
+        <button id = 'test1-btn'>Send POST</button>
+        <button id = 'test2-btn'>Send GET</button>
         <script>
             $(document).ready(function() {
-                $('#test-btn').click(function() {
+                $('#test1-btn').click(function() {
                     $.ajax({
                         url: 'http://localhost:8000/users/add',
                         method: 'POST',
@@ -22,6 +23,23 @@ module.exports =
                         console.log(data);
                     }).fail(function(jqXhr, status, error) {
                         console.log('Fallimento!');
+                        console.log(jqXhr);
+                        console.log(status + ": " + error);
+                    });
+                });
+
+                $('#test2-btn').click(function() {
+                    $.ajax({
+                        url: 'http://localhost:8000/users/get/email@email.com',
+                        method: 'GET',
+                        data: {},
+                        dataType: 'json'
+                    }).done(function(data) {
+                        console.log('Successo!');
+                        console.log(data);
+                    }).fail(function(jqXhr, status, error) {
+                        console.log('Fallimento!');
+                        console.log(jqXhr);
                         console.log(status + ": " + error);
                     });
                 });
