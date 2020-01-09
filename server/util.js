1cd46f9e4fa3a466d9d19a4a842c0dae94795a87
Saimonz <grillini.simo@gmail.com>
2020-01-09 17:07:06 +0100

    Modulo con utilities.

---
+++ server/util.js
@@ -0,0 +1,5 @@
+var SUCCESS = '[✔]';
+var FAIL = '[✖]';
+
+var logSuccess = function(...strings) { console.log(`${SUCCESS} `, strings); }
+var logFail = function(...strings) { console.log(`${FAIL} `, strings); }
