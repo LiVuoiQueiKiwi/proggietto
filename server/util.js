c539ee193a9093511ca33be03aaa86784283508e
Saimonz <grillini.simo@gmail.com>
2020-01-12 20:29:29 +0100

    Aggiunta funzione di debug su console.

--- server/util.js
+++ server/util.js
@@ -1,5 +1,14 @@
 var SUCCESS = '[✔]';
 var FAIL = '[✖]';
 
-module.exports.logSuccess = function(...strings) { console.log(`${SUCCESS} `, ...strings); }
-module.exports.logFail = function(...strings) { console.log(`${FAIL} `, ...strings); }
+module.exports.logSuccess = function(...strings) {
+    console.log(`${SUCCESS} `, ...strings);
+}
+
+module.exports.logFail = function(...strings) {
+    console.log(`${FAIL} `, ...strings);
+}
+
+module.exports.debug = function(variable) {
+    console.log('DEBUG: (', typeof variable, ')', variable);
+}
