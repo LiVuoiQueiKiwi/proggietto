53aab7e542488c52ec1e217d72c8850c01880d29
Saimonz <grillini.simo@gmail.com>
2020-01-16 23:05:05 +0100

    Aggiunti nuovi test.

--- server/jqueryPostPage.js
+++ server/jqueryPostPage.js
@@ -6,7 +6,9 @@ module.exports =
     </head>
     <body>
         <button id = 'test1-btn'>Send POST</button>
-        <button id = 'test2-btn'>Send GET</button>
+        <button id = 'test2-btn'>GET dummy user.</button>
+        <button id = 'test3-btn'>GET all users.</button>
+        <button id = 'test4-btn'>LOGIN</button>
         <script>
             $(document).ready(function() {
                 $('#test1-btn').click(function() {
@@ -19,12 +21,9 @@ module.exports =
                         },
                         dataType: 'json'
                     }).done(function(data) {
-                        console.log('Successo!');
-                        console.log(data);
+                        handleSuccess(data);
                     }).fail(function(jqXhr, status, error) {
-                        console.log('Fallimento!');
-                        console.log(jqXhr);
-                        console.log(status + ": " + error);
+                        handleFail(jqXhr, status, error);
                     });
                 });
 
@@ -35,15 +34,68 @@ module.exports =
                         data: {},
                         dataType: 'json'
                     }).done(function(data) {
-                        console.log('Successo!');
-                        console.log(data);
+                        handleSuccess(data);
                     }).fail(function(jqXhr, status, error) {
-                        console.log('Fallimento!');
-                        console.log(jqXhr);
-                        console.log(status + ": " + error);
+                        handleFail(jqXhr, status, error);
+                    });
+                });
+
+                $('#test3-btn').click(function() {
+                    $.ajax({
+                        url: 'http://localhost:8000/users/get',
+                        method: 'GET',
+                        data: {},
+                        dataType: 'json'
+                    }).done(function(data) {
+                        handleSuccess(data);
+                    }).fail(function(jqXhr, status, error) {
+                        handleFail(jqXhr, status, error);
+                    });
+                });
+
+                $('#test4-btn').click(function() {
+                    $.ajax({
+                        url: 'http://localhost:8000/users/login',
+                        method: 'POST',
+                        data: {
+                            email: 'email@email.com',
+                            password: 'ciao'
+                        },
+                        dataType: 'json'
+                    }).done(function(data) {
+                        handleSuccess(data);
+                    }).fail(function(jqXhr, status, error) {
+                        handleFail(jqXhr, status, error);
+                    });
+                });
+
+                $('#test5-btn').click(function() {
+                    $.ajax({
+                        url: 'http://localhost:8000/users/delete',
+                        method: 'DELETE',
+                        data: {
+                            email: 'email@email.com'
+                        },
+                        dataType: 'json'
+                    }).done(function(data) {
+                        handleSuccess(data);
+                    }).fail(function(jqXhr, status, error) {
+                        handleFail(jqXhr, status, error);
                     });
                 });
             });
+
+
+            var handleSuccess = function(data) {
+                console.log('Successo!');
+                console.log(data);
+            }
+
+            var handleFail = function(jqXhr, status, error) {
+                console.log('Fallimento!');
+                console.log(jqXhr);
+                console.log(status + ": " + error);
+            }
         </script>
     </body>
 </html>`;
