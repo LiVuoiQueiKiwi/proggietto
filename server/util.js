62c7b923af73e55d7365f90ca7fda0878184fc26
Saimonz <grillini.simo@gmail.com>
2020-01-28 01:25:53 +0100

    Nuova funzione di debug.

--- server/util.js
+++ server/util.js
@@ -1,3 +1,5 @@
+const DEBUG_ON = 1;
+
 var SUCCESS = '[✔]';
 var FAIL = '[✖]';
 
@@ -13,4 +15,12 @@ module.exports.debug = function(...variables) {
     variables.forEach(function(item) {
         console.log('DEBUG: (', typeof item, ')', item);
     });
+}
+
+module.exports.terminal = function(string) {
+    if (DEBUG_ON) {
+        var now = new Date();
+        timestamp = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+        console.log(`[${timestamp}]  ${string}`);
+    }
 }
