f0b10f20db0acfb802526e5d992bf54c920ce990
Saimonz <grillini.simo@gmail.com>
2020-02-04 17:59:18 +0100

    Refactoring del codice.

--- server/util.js
+++ server/util.js
@@ -4,13 +4,6 @@ var SUCCESS = '[✔]';
 var FAIL = '[✖]';
 
 module.exports = {
-    logSuccess: function(...strings) {
-        console.log(`${SUCCESS} `, ...strings);
-    },
-
-    logFail: function(...strings) {
-        console.log(`${FAIL} `, ...strings);
-    },
 
     debug: function(...variables) {
         variables.forEach(function(item) {
@@ -18,34 +11,48 @@ module.exports = {
         });
     },
 
-    terminal: function(string) {
+    terminal: function(string, symbol = '   ') {
         if (DEBUG_ON) {
             var now = new Date();
             timestamp = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-            console.log(`[${timestamp}]  ${string}`);
+            console.log(`[${timestamp}]${symbol}  ${string}`);
         }
     },
 
-    getDistance: function(origin, destination) {
-        // return distance in meters
-
-        var lon1 = toRadian(OLC_Coords(origin).lng),
-            lat1 = toRadian(OLC_Coords(origin).lat),
-            lon2 = toRadian(OLC_Coords(destination).lng),
-            lat2 = toRadian(OLC_Coords(destination).lat);
-
-        var deltaLat = lat2 - lat1;
-        var deltaLon = lon2 - lon1;
-
-        var a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon/2), 2);
-        var c = 2 * Math.asin(Math.sqrt(a));
-        const EARTH_RADIUS = 6371;
-        return c * EARTH_RADIUS * 1000;
+    logSuccess: function(string) {
+        this.terminal(string, SUCCESS);
     },
+
+    logFail: function(string) {
+        this.terminal(string, FAIL);
+    },
+
+    // getDistance: function(origin, destination) {
+    //     // return distance in meters
+    //
+    //     var lon1 = toRadian(OLC_Coords(origin).lng),
+    //         lat1 = toRadian(OLC_Coords(origin).lat),
+    //         lon2 = toRadian(OLC_Coords(destination).lng),
+    //         lat2 = toRadian(OLC_Coords(destination).lat);
+    //
+    //     var deltaLat = lat2 - lat1;
+    //     var deltaLon = lon2 - lon1;
+    //
+    //     var a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon/2), 2);
+    //     var c = 2 * Math.asin(Math.sqrt(a));
+    //     const EARTH_RADIUS = 6371;
+    //     return c * EARTH_RADIUS * 1000;
+    // },
 
     /**
      * Gestisce la reject() di una Promise.
      */
     handlePromiseRejection: function(error) {
-        this.logFail(error);
+        module.exports.logFail(error);
     }
+};
+
+
+// function OLC_Coords(olc){
+//     return {lat: OpenLocationCode.decode(olc).latitudeCenter , lng: OpenLocationCode.decode(olc).longitudeCenter };
+// }
