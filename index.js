439d58406ab5e3c4d18e2b2809feda975d9182f8
Saimonz <grillini.simo@gmail.com>
2020-02-11 10:39:27 +0100

    Rimozione del codice in eccesso.

--- index.js
+++ index.js
@@ -67,17 +67,9 @@ var sess;
  */
 const NO_AUTH_MESSAGE = 'Accesso API non autorizzato';
 
-// app.get('/',function(req,res){
-//     sess=req.session;
-//     /*
-//     * Here we have assign the 'session' to 'sess'.
-//     * Now we can create any number of session variable we want.
-//     * in PHP we do as $_SESSION['var name'].
-//     * Here we do like this.
-//     */
-//     sess.email; // equivalent to $_SESSION['email'] in PHP.
-//     sess.username; // equivalent to $_SESSION['username'] in PHP.
-// });
+
+
+
 
 
 app.get('/', sendMainPage);
@@ -102,9 +94,11 @@ function sendMainPage(request, response) {
 }
 
 
+
 // #############################################################################
 //   SEZIONE PER GLI UTENTI.
 // #############################################################################
+
 
 
 app.post('/users/login', function(request, response) {
@@ -196,7 +190,7 @@ app.put('/users', function(request, response) {
 
 
 /*****/
-// SOLO PER TESTING.
+/*       SOLO PER TESTING. */
 /*****/
 app.get('/users', function(request, response) {
 
@@ -232,10 +226,6 @@ app.get('/users/:email', function(request, response) {
 
 
 
-/**
- * TODO: Da aggiungere il controllo sulla sessione se l'utente che accede
- * all'API e' lo stesso (stessa email eliminata) ed e' loggato.
- */
 app.delete('/users/:email', function(request, response) {
 
     /*
@@ -414,6 +404,7 @@ app.get('/clips/:clipId', function(request, response) {
 });
 
 
+
 /*
  * Restituisce TUTTE le clip di un utente.
  */
@@ -525,67 +516,6 @@ app.get('/private', function(request, response) {
 
 
 
-/*
- * Restituisce le clip piu' vicine all'utente.
- */
-// app.get('/nearest/:location', function(request, response) {
-//     /*
-//      * Inizializzo la sessione.
-//      */
-//     sess = request.session;
-//
-//     /*
-//      * Inizializzo la risposta.
-//      */
-//     var serverResponse = new ApiResponse();
-//
-//     /*
-//      * Prelevo gli argomenti della richiesta.
-//      */
-//     var location = request.params.location;
-//
-//     /*
-//      * Solo un utente autenticato puo' eliminare il proprio account.
-//      */
-//     if (isUserLogged(sess)) {
-//         util.logSuccess(`Richiesta GET ricevuta. Prelievo le clip piu' vicine a [${location}] all'utente ${sess.userEmail}`);
-//
-//         /*
-//          * Inizializzo il risultato.
-//          */
-//         var filteredClips = [];
-//
-//         /*
-//          * Prelievo tutte le clip.
-//          */
-//         db.getClips(sess.userId).then(function(result) {
-//
-//             /*****/ util.debug(result);
-//
-//             if (result.success) {
-//                 result.content.forEach(function(clip) {
-//                     // if (util.getDistance(location, clip.geoloc)) {
-//                         clip.TEST = util.getDistance(location, clip.geoloc);
-//                         filteredClips.push(clip);
-//                     // }
-//                 });
-//
-//                 result.content = filteredClips;
-//             } else {
-//                 /*
-//                  * Rispondo al client con il risultato (errore).
-//                  */
-//                 sendToClient(response, result);
-//             }
-//         }, util.handlePromiseRejection);
-//     } else {
-//         serverResponse.message = NO_AUTH_MESSAGE;
-//         sendToClient(response, serverResponse);
-//     }
-// });
-
-
-
 app.delete('/clips/:id', function(request, response) {
     /*
      * Inizializzo la sessione.
@@ -652,6 +582,7 @@ app.get('/fs', function(request, response) {
 // #############################################################################
 
 
+
 /*
  * Il web server rimane in ascolto sulla porta specificata.
  */
@@ -698,188 +629,3 @@ var toJson = function(data) {
 var isUserLogged = function(session) {
     return session.userIsLogged && session.userEmail;
 }
-
-
-
-
-//
-// // Configure our HTTP server to respond with Hello World to all requests.
-// var server = http.createServer(function(request, response) {
-// 	// var parameters = url.parse( response.url, true );
-// 	// fs.readFile
-// 	// (
-// 	// 	'index.html',
-// 	// 	function( error, data )
-// 	// 	{
-// 	// 		response.writeHead( 200, { 'Content-Type': 'text/html' } );
-// 	// 		response.write( data );
-// 	// 		response.end();
-// 	// 	}
-// 	// );
-//
-// 	var example_url = 'https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash';
-// 	var parsed_url = url.parse( request.url, true );
-// 	var splittedUrl = parsed_url.pathname.split( '/' );
-//
-//
-// 	var final_content =
-// 	{
-// 		agent: "NODE SERVER",
-// 		content:
-// 		{
-// 			requestURL: request.url,
-// 			//requestURL: example_url,
-// 			parseRequest: parsed_url,
-// 			splittedURL: splittedUrl
-// 			//parseRequest: url.parse( example_url, true )
-// 		}
-// 	};
-//
-// 	//response.writeHead( 200, { 'Content-Type': 'application/json' } );
-// 	//response.end( JSON.stringify( final_content ) );
-//
-//
-//
-// 	// Esamino gli elementi dell'URL splittati.
-// 	if (splittedUrl.length >= 2) {
-// 		// In questo caso e' stato inserito un percorso composto dopo l'host.
-// 		if (!is_undefined(splittedUrl[1])) {
-// 			switch (splittedUrl[1]) {
-// 				case '':
-// 				case 'index':
-// 					response_with_index( response );
-// 					break;
-//
-// 				case 'test':
-// 					break;
-//
-// 				default:
-// 					response_with_error( response, 'An error occured parsing the URL string.' );
-// 			}
-// 		}
-// 	} else {
-// 		// In questo caso l'URL e' quello di base e quindi restituisco l'index.html
-// 		response_with_index( response );
-// 	}
-//
-//
-// });
-//
-// // Listen on port 8000, IP defaults to 127.0.0.1
-// server.listen( SERVER_PORT );
-//
-// // Put a friendly message on the terminal
-// console.log( `Server running at http://127.0.0.1:${ SERVER_PORT }/` );
-//
-//
-// function response_with_index ( response )
-// {
-// 	require( 'fs' ).readFile
-// 	(
-// 		`${ DOC_FOLDER }/index.html`,
-// 		function( error, data )
-// 		{
-// 			response.writeHead( 200, { 'Content-Type': 'text/html' } );
-// 			response.write( data, function ( error ) { response.end(); } );
-// 			response.end();
-// 		}
-// 	);
-// }
-//
-//
-//
-// function response_with_error ( response, errorMessage )
-// {
-// 	response.writeHead( 200, { 'Content-Type': 'text/html' } );
-// 	response.write(
-// 		`<!DOCTYPE html>
-// 		<html>
-// 			<head>
-// 				<title>Where am I?</title>
-// 			</head>
-// 			<body>
-// 				<style>
-// 				html, body { width: 100vw; height: 100vh; margin: 0; padding: 0; display: flex; flex-direction: column; align-items: center; justify-content: center; }
-// 				img { width: 100%; }
-// 				</style>
-// 				<h1>${errorMessage}</h1>
-// 			</body>
-// 		</html>`,
-// 		function(error) {
-//             response.end();
-//         }
-//     );
-// 	response.end();
-// }
-//
-//
-//
-// function failed_to_write ( error )
-// {
-//
-// }
-//
-//
-// function is_undefined ( variable )
-// {
-// 	return null == variable;
-// }
-
-
-//  https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shitpostbot.com%2Fimg%2Fsourceimages%2Fjohnny-sins-with-a-moustache-57fcd536bedcb.png&f=1&nofb=1
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-ESEMPIO fs.appendFile()
-
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
-
-
-ESEMPIO fs.open()
-
-fs.open('mynewfile2.txt', 'w', function (err, file) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
-
-
-ESEMPIO fs.writeFile()
-
-fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
-
-
-ESEMPIO fs.unlink()
-
-fs.unlink('mynewfile2.txt', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
-
-
-ESEMPIO fs.rename()
-
-fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-  if (err) throw err;
-  console.log('File Renamed!');
-});
-
-*/
