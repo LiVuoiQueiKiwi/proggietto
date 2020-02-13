af019e6543664d044b87a16f408c8a13739ba24b
GiacomoNeriUnibo <giacomo.neri5@studio.unibo.it>
2020-02-13 16:11:03 +0100

    checkmate

--- asset/youtubeApi.js
+++ asset/youtubeApi.js
@@ -1,51 +1,63 @@
-API_KEY=''
+
 
 function uploadVideo(file, metadata) {
-
-      description=metadata.get('geoloc')+":"+metadata.get('purpose')+":"+metadata.get('language')+":"+metadata.get('content')
-      if(metadata.get('audience')!='')
-        description+=":"+metadata.get('audience')
-      if(metadata.get('detail')!='')
-        description+=":"+metadata.get('detail')
-      privacy="private"
-      if(metadata.get('public')==1)
-        privacy="public"
-
-      metadata_formatted=
-      {
-        "snippet": {
-          "categoryId": "22",
-          "description": description,
-          "title": metadata.get('title')
-        },
-        "status": {
-          "privacyStatus": privacy
+	user = GoogleAuth.currentUser.get();
+	if(user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.upload')){
+		if(file==null){
+            alert("Nessun file");
         }
-      }
-      //alert(metadata_formatted.snippet.description)
+		else{
+			  description=metadata.get('geoloc')+":"+metadata.get('purpose')+":"+metadata.get('language')+":"+metadata.get('content')
+			  if(metadata.get('audience')!='')
+				description+=":"+metadata.get('audience')
+			  if(metadata.get('detail')!='')
+				description+=":"+metadata.get('detail')
+			  privacy="private"
+			  if(metadata.get('public')==1)
+				privacy="public"
 
-	var auth = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
-	//var auth = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token
-	var form = new FormData();
-	var video_meta = new Blob([JSON.stringify(metadata_formatted)], {type: 'application/json'});
+			  var metadata_formatted=
+			  {
+				"kind": 'youtube#video',
+				"snippet": {
+				  "categoryId": "22",
+				  "description": description,
+				  "title": metadata.get('title')
+				},
+				"status": {
+				  "privacyStatus": privacy,
+				  "embeddable": true
+				}
+			  }
+			  //alert(metadata_formatted.snippet.description)
 
-	form.append('video', video_meta);
-	form.append('mediaBody', file);
+			//gapi.auth2.init()
+			var auth = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+			//alert(auth)
+			//var auth = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token
+			var form = new FormData();
+			var video_meta = new Blob([JSON.stringify(metadata_formatted)], {type: 'application/json'});
 
-	$.ajax({
-		url: 'https://www.googleapis.com/upload/youtube/v3/videos?part=snippet,status&key='+API_KEY,
-		data: form,
-		cache: false,
-		contentType: false,
-		processData: false,
-		method: 'POST',
-		success: function(data){
-      uploadVideoSuccess(data, metadata)
-    },
-		error: function(error){
-        alert(error)
-    }
-	})
+			form.append('video', video_meta);
+			form.append('mediaBody', file);
+
+			$.ajax({
+				url: 'https://www.googleapis.com/upload/youtube/v3/videos?access_token='+ encodeURIComponent(auth) + '&part=snippet,status',
+				data: form,
+				cache: false,
+				contentType: false,
+				processData: false,
+				method: 'POST',
+				success: function(data){
+				alert('ok')
+			  //uploadVideoSuccess(data, metadata)
+			},
+				error: function(error){
+				alert(error)
+			}
+			})
+		}
+	}
 }
 
 function uploadVideoSuccess(idVideo, formData){
@@ -53,14 +65,22 @@ function uploadVideoSuccess(idVideo, formData){
     formData.append('idVideo', idVideo)
     $.ajax(
       {
-        url: '', //inserire link del server (Funzione: uploadClip)
-        type: 'POST',
+        url: 'clips', //inserire link del server (Funzione: uploadClip)
+        type: 'PUT',
         dataType: 'json',
         data: formData,
         processData: false,	// Evita che Jquery faccia operazioni sui dati.
         contentType: false	// Evita che Jquery faccia operazioni sui dati.
-      }
-    )
+      }).done(function(data) {
+			if(data.success){
+				alert('Video caricato con successo!')
+			} else{
+				alert(data.message);
+			}
+		}).fail(function(jqXhr, status, error){
+			console.log(error)
+			// Gestione dell'errore AJAX.
+		});
 }
 
 
@@ -87,14 +107,22 @@ function deleteVideoSuccess(id){
 
     $.ajax(
       {
-        url: '', //inserire link del server (Funzione: deleteClip)
-        type: 'POST',
+        url: 'clips/:'+id, //inserire link del server (Funzione: deleteClip)
+        type: 'DELETE',
         dataType: 'json',
-        data: id,
         processData: false,	// Evita che Jquery faccia operazioni sui dati.
         contentType: false	// Evita che Jquery faccia operazioni sui dati.
       }
-    )
+    ).done(function(data) {
+		if(data.success){
+			alert('Video eliminato con successo!')
+		} else{
+			alert(data.message);
+		}
+	}).fail(function(jqXhr, status, error){
+		console.log(error)
+		// Gestione dell'errore AJAX.
+	});
 }
 
 
@@ -148,14 +176,23 @@ function updateVideoSuccess(formData){
 
     $.ajax(
       {
-        url: '', //inserire link del server (Funzione: updateClip)
-        type: 'POST',
+        url: 'clips', //inserire link del server (Funzione: updateClip)
+        type: 'UPDATE',
         dataType: 'json',
         data: formData,
         processData: false,	// Evita che Jquery faccia operazioni sui dati.
         contentType: false	// Evita che Jquery faccia operazioni sui dati.
       }
-    )
+    ).done(function(data) {
+		if(data.success){
+			alert('Video modificato con successo!')
+		} else{
+			alert(data.message);
+		}
+	}).fail(function(jqXhr, status, error){
+		console.log(error)
+		// Gestione dell'errore AJAX.
+	});
 }
 
 
@@ -195,15 +232,26 @@ function publishVideo(id, title) {
 }
 
 function publishVideoSuccess(id){
-
+	formData=new formData()
+	formData.append("published", "1")
+	formData.append("id", id)
     $.ajax(
       {
         url: '', //inserire link del server (Funzione: setClipPublished)
-        type: 'POST',
+        type: 'UPDATE',
         dataType: 'json',
         data: formData,
         processData: false,	// Evita che Jquery faccia operazioni sui dati.
         contentType: false	// Evita che Jquery faccia operazioni sui dati.
       }
-    )
+    ).done(function(data) {
+		if(data.success){
+			alert('Video pubblicato con successo!')
+		} else{
+			alert(data.message);
+		}
+	}).fail(function(jqXhr, status, error){
+		console.log(error)
+		// Gestione dell'errore AJAX.
+	});
 }
